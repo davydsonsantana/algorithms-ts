37ed3de190682a713a7a951412dd7eeff3d3fe4c
Davydson Fellipe Verri Santana <davys.fellipe@gmail.com>
2022-07-20 12:02:00 -0300

    test: add unit test on roman-to-int problem

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { longestCommonPrefix } from '@problems/longest-common-prefix/longest-common-prefix'
+import { longestCommonPrefix } from '@problems/longest-common-prefix/longest-common-prefix-01'
 import { romanToInt } from '@problems/roman-to-int/roman-to-int'
 
 //romanToInt('MCMXCIV')
+
 let arr = ['dog', 'racecar', 'car', 'doug']
 console.log('Common: ' + longestCommonPrefix(arr))
---
+++ tests/unit/problems/roman-to-int/roman-to-int.spec.ts
@@ -0,0 +1,17 @@
+import { romanToInt } from "@problems/roman-to-int/roman-to-int"
+
+describe("Problem = roman_number_to_int", () => {
+  const cases = [
+    ["III", 3],
+    ["LVIII", 58],
+    ["MCMXCIV", 1994],
+  ]
+
+  test.each(cases)(
+    "given roman number %p as argument, return int %p",
+    (firstArg, secondArg) => {
+      var result = romanToInt(firstArg.toString())
+      expect(result).toBe(secondArg)
+    }
+  )
+})
